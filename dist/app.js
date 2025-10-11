b43ecd9d9da84aa7e8dd528ea18bc5e489e26a3e
tanver909 <tanveermahidi@gmial.com>
2025-10-11 16:55:53 +0600

    live link added

--- dist/app.js
+++ dist/app.js
@@ -17,6 +17,7 @@ app.use((0, cookie_parser_1.default)());
 app.use((0, cors_1.default)({
     origin: [
         "https://jcstrading.vercel.app",
+        "http://localhost:3000"
     ],
     credentials: true
 }));
--- src/app.ts
+++ src/app.ts
@@ -16,6 +16,7 @@ app.use(cookieParser())
 app.use(cors({
   origin: [
     "https://jcstrading.vercel.app",
+    "http://localhost:3000"
     ],
     credentials:true
 }))
